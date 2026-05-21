bb9b2a87ccbfb833caed74d2c50d064cf42390a6
Denis Mokin <denmokin@gmail.com>
2026-05-22 00:00:04 +0300

    Async store actions Refactor

--- src/store/actions/toy.actions.js
+++ src/store/actions/toy.actions.js
@@ -4,45 +4,47 @@ import { store } from '../store.js'
 // import { toyService } from '../../service/toy.service.local.js'
 import { toyService } from '../../service/toy.service.js'
 
-export function loadToys() {
+export async function loadToys() {
     const filterBy = store.getState().toyModule.filterBy
     store.dispatch({ type: SET_IS_LOADING, isLoading: true })
-    return toyService.query(filterBy)
-        .then(toys => {
-            store.dispatch({ type: SET_TOYS, toys })
-        })
-        .catch(err => {
-            console.log('toy action - Cannot Load Toys', err)
-            throw err
-        })
-        .finally(store.dispatch({ type: SET_IS_LOADING, isLoading: false }))
+
+    try {
+        const toys = await toyService.query(filterBy)
+        store.dispatch({ type: SET_TOYS, toys })
+    }
+    catch (err) {
+        console.log('toy action - Cannot Load Toys', err)
+        throw err
+    }
+    finally {
+        store.dispatch({ type: SET_IS_LOADING, isLoading: false })
+    }
 }
 
 
-export function removeToy(toyId) {
-    return toyService.remove(toyId)
-        .then(() => {
-            store.dispatch({ type: REMOVE_TOY, toyId })
-        })
-        .catch(err => {
-            console.log(`toy action - Cannot REmove Toy:${toyId}`, err)
-            throw err
-        })
+export async function removeToy(toyId) {
+    try {
+        await toyService.remove(toyId)
+        store.dispatch({ type: REMOVE_TOY, toyId })
+    }
+    catch (err) {
+        console.log(`toy action - Cannot REmove Toy:${toyId}`, err)
+        throw err
+    }
 }
 
-export function saveToy(toy) {
+export async function saveToy(toy) {
 
     const type = toy._id ? UPDATE_TOY : ADD_TOY
 
-    return toyService.save(toy)
-        .then((savedToy) => {
-            store.dispatch({ type, toy: savedToy })
-            return saveToy
-        })
-        .catch(err => {
-            console.log(`toy action - Cannot ${type}`, err)
-            throw err
-        })
+    try {
+        const savedToy = await toyService.save(toy)
+        store.dispatch({ type, toy: savedToy })
+    }
+    catch (err) {
+        console.log(`toy action - Cannot ${type}`, err)
+        throw err
+    }
 }
 
 export function setFilterBy(filterBy) {
--- src/store/actions/user.actions.js
+++ src/store/actions/user.actions.js
@@ -1,35 +1,40 @@
 import { SET_USER } from '../reducers/user.reducer.js'
 import { store } from '../store.js'
 
-import { userService } from '../../service/user.service.js'
 import { authService } from '../../service/auth.service.js'
 
-export function login(credentials) {
-    return authService.login(credentials)
-        .then(user => store.dispatch({ type: SET_USER, user }))
-        .catch(err => {
-            console.log('user action -> Cannot login', err)
-            throw err
-        })
+export async function login(credentials) {
+    try {
+        const user = await authService.login(credentials)
+        store.dispatch({ type: SET_USER, user })
+        return user
+    }
+    catch (err) {
+        console.log('user action -> Cannot login', err)
+        throw err
+    }
 }
 
-
-export function signup(credentials) {
-    return authService.signup(credentials)
-        .then(user => store.dispatch({ type: SET_USER, user }))
-        .catch(err => {
-            console.log('user action -> Cannot signup', err)
-            throw err
-        })
-
+export async function signup(credentials) {
+    try {
+        const user = await authService.signup(credentials)
+        store.dispatch({ type: SET_USER, user })
+        return user
+    }
+    catch (err) {
+        console.log('user action -> Cannot signup', err)
+        throw err
+    }
 }
 
-export function logout() {
-    return authService.logout()
-        .then(() => store.dispatch({ type: SET_USER, user: null }))
-        .catch(err => {
-            console.log('user action -> Cannot logout', err)
-            throw err
-        })
-
+export async function logout() {
+    try {
+        const user = await authService.logout()
+        store.dispatch({ type: SET_USER, user: null })
+        return user
+    }
+    catch (err) {
+        console.log('user action -> Cannot logout', err)
+        throw err
+    }
 }
